950d88ea74c64764b451c5cb4e0e9e08a0a6716e
mitu1403 <mmitu163069@bscse.uiu.ac.bd>
2025-01-01 13:38:31 +0600

    create and load fake data, chatgpt json, online api

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -1,13 +1,17 @@
 import { adding, multiply, divide as bhaag } from "../Utils/Calculate";
-function Watch() {
+function Watch({watch}) {
+  
+  const {id, model, price} = watch
   const sum = adding(20, 30);
   const gun = multiply(20, 30);
   const bhag = bhaag(sum, gun);
   return (
     <div>
-      <p>{sum}</p>
+      {/* <p>{sum}</p>
       <p>{gun}</p>
-      <p>{bhag}</p>
+      <p>{bhag}</p> */}
+      <p>Model Name: {model}</p>
+      <small>Price: {price}</small>
     </div>
   );
 }
